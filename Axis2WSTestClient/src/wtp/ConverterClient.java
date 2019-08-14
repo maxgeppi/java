4e9149a8087999bc0116caacb7d4d846c5186cd8
Massimiliano Geppi <maxgeppi@tiscali.it>
2019-08-14 16:33:48 +0200

    Modifche di indentazione.

--- Axis2WSTestClient/src/wtp/ConverterClient.java
+++ Axis2WSTestClient/src/wtp/ConverterClient.java
@@ -4,25 +4,28 @@ import java.rmi.RemoteException;
 
 import org.apache.axis2.AxisFault;
 
-public class ConverterClient {
-
-	public static void main(String[] args) {
+public class ConverterClient 
+{
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-        try {
+        try 
+        {
             float celsiusValue = 100;
             ConverterStub stub = new ConverterStub();
             ConverterStub.CelsiusToFarenheit cc2f = new ConverterStub.CelsiusToFarenheit();
             cc2f.setCelsius(celsiusValue);
             ConverterStub.CelsiusToFarenheitResponse cc2f_res = stub.celsiusToFarenheit(cc2f);
-            
             System.out.println("Celsius : "+celsiusValue+" = "+"Farenheit : " + cc2f_res.get_return());
-    } catch (AxisFault e) {
+        } 
+        catch (AxisFault e) 
+        {
             e.printStackTrace();
-    } catch (RemoteException e) {
+        } 
+        catch (RemoteException e) 
+        {
             e.printStackTrace();
-    }		
-
+        }		
 	}
-
 }
 
